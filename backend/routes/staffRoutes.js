3e577c63b2d2275f369285a5202d5f6a921b7ab6
fasnaa <sinfasna@gmail.com>
2025-10-13 03:25:11 +0530

    fix: staff data fetch

--- backend/routes/staffRoutes.js
+++ backend/routes/staffRoutes.js
@@ -7,9 +7,15 @@ const router = express.Router();
 router.post("/register", verifyToken, authorizeRoles("Manager","Admin"), registerStaff); // Only Manager can register others
 router.post("/login", loginStaff);
 
-router.get("/me", verifyToken, (req, res) => {
-  res.status(200).json({ user: req.user });
+router.get("/me", verifyToken, async (req, res) => {
+  try {
+    const user = await Staff.findById(req.user.id).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
-        
